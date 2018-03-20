2e0d8e4888c57c6a6fa4dd7c7cac68b8a7d3a279
yavuzs <yavuzselim@sabanciuniv.edu>
2018-03-20 20:44:31 +0300

    Add home page and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Navigation from './Navigation';
+import Home from './Home';
+import Login from './Login';
 
 class App extends Component {
   render() {
     return (
-      
-      <div className="App">
-        <Navigation/>
-        <p>U need a f***ing budget</p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navigation/>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' component={Login} />
+            <Route render={function () {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,31 +1,37 @@
 import React, { Component } from 'react';
 import { Nav, Navbar, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.css'
+import { NavLink } from 'react-router-dom';
+
 
 class Navigation extends Component {
   render(){
       return (
-      <Navbar className="navbar navbar-inverse navbar-fixed-top">
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="#home">UNAFB</a>
-        </Navbar.Brand>
-      </Navbar.Header>
-      <Nav className="nav navbar-nav">
-        <NavItem eventKey={1} href="#">
-          Link
-        </NavItem>
-        <NavItem eventKey={2} href="#">
-          Link
-        </NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.4}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
+      <Navbar className="navbar navbar-inverse">
+        <Navbar.Header>
+          <Navbar.Brand>
+            <NavLink exact activeClassName='active' to='/'>UNAFB</NavLink>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav className="nav navbar-nav">
+          <NavItem eventKey={1} href="#">
+            Link
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            Link
+          </NavItem>
+          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.4}>Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="/login">
+            Login
+          </NavItem>
+        </Nav>
       </Navbar>);
   }
 }
