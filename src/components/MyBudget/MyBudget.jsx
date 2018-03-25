d5d780e45cf36a3f3bc9067cb5ca9a37b5abcbec
yavuzs <yavuzselim@sabanciuniv.edu>
2018-03-25 14:17:47 +0300

    Add react-vis

--- src/components/MyBudget/MyBudget.jsx
+++ src/components/MyBudget/MyBudget.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries } from 'react-vis';
 
 class MyBudget extends Component {
   render() {
     return (
-      <div className="MyBudget">
-        <h1> My Budget Page </h1>
+      <div className="row">
+        <div className="col-md-3" />
+        <div className="MyBudget col-md-6">
+          <XYPlot
+            width={600}
+            height={300}
+          >
+            <HorizontalGridLines />
+            <LineSeries
+              color="red"
+              data={[
+                   { x: 1, y: 10 },
+                   { x: 2, y: 5 },
+                   { x: 3, y: 11 },
+                   { x: 4, y: 21 },
+                   { x: 5, y: 29 },
+                   { x: 6, y: 2 },
+                   { x: 7, y: 17 },
+                   { x: 8, y: 35 },
+                   { x: 9, y: 12 },
+                   { x: 10, y: 11 },
+                   { x: 11, y: 56 },
+                   { x: 12, y: 67 },
+                  ]}
+            />
+            <XAxis />
+            <YAxis />
+          </XYPlot>
+        </div>
+        <div className="col-md-3" />
       </div>
     );
   }
